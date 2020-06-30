23dd88e1f889b6fa8fb20af11ec11357c8122dbc
unknown <flashwriter74@gmail.com>
2020-06-29 20:40:33 -0400

    Signed-off-by: unknown <flashwriter74@gmail.com>

--- creditapp/src/globals.js
+++ creditapp/src/globals.js
@@ -1,9 +1,9 @@
 const globalVars = {
     "isLocalConn": "false",
     //express handlers
-    "localServPort": "http://localhost:5000/",
-    "localServPort_base": "http://localhost:5000",
-    "remoteServPort": "https://cors-anywhere.herokuapp.com/https://damp-reaches-51152.herokuapp.com/",
+    "localServPort": "https://cors-anywhere.herokuapp.com/https://mern-shell.herokuapp.com/",
+    "localServPort_base": "https://cors-anywhere.herokuapp.com/https://mern-shell.herokuapp.com",
+    "remoteServPort": "https://cors-anywhere.herokuapp.com/https://mern-shell.herokuapp.com/",
     //react handlers
     "localExpPort": "http://localhost:3000/",
     "localExpPort_base": "http://localhost:3000",
